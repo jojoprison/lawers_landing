b8cb927c5ffcc9125a1f7a23090ada298e72f6cb
MaxLestor <MaxLestor@github.com>
2020-11-04 17:03:43 +0600

    фыв
    фывфыв

--- 1.2.php
+++ 1.2.php
@@ -4,7 +4,7 @@
         <?php include_once 'fixed_nav.php'; ?>
     </div>
     <?php include_once 'baza_menu.php'; ?>
-    <br/>
+    </br>
     <div id="content">
         <div class="inner-content">
             <h2 class="zagolovok_h2">
---
+++ 4.2.php
@@ -0,0 +1,23 @@
+<body>
+<div id="wrapeper">
+    <div id="header">
+        <?php include_once 'fixed_nav.php'; ?>
+    </div>
+    <?php include_once 'baza_menu.php'; ?>
+    <br/>
+    <div id="content">
+        <div class="inner-content">
+            <h2 class="zagolovok_h2">
+                Вернуть деньги из бинарных опционов
+            </h2>
+
+            <div class="container proba">
+                Информация будет размещена в течении 30 дней. Мы подготавливаем для Вас материал.
+            </div>
+        </div>
+    </div>
+</div>
+
+<? include_once 'footer.php' ?>
+
+</body>

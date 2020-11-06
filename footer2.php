aa7dd6ed47f86298792a317a8682cfcfb0b67ea2
MaxLestor <MaxLestor@github.com>
2020-11-07 03:09:32 +0600

    0.8
    NEW DESIGN

---
+++ footer2.php
@@ -0,0 +1,72 @@
+   <!--FOOTER-->
+   <footer class="navbar-fixed-bottom" style="background-color:#EAF1FC; height: auto">
+    <div id="sticky-footer" class="py-4 text-dark-50" style="background-color: #f5f5f5">
+      
+      <div class="container" >
+        <div class="row">
+          <div class="col-sm center" >
+            <img src="logo.png" width="30%" alt="">
+            <ul style="list-style-type:none; display: flex; ">
+                <Li ><a href="index.html" style="color:black;padding-right: 1rem;"> Главная  </a> </Li>
+              <Li ><a href="tartifs.html" style="color:black;padding-right: 1rem;"> Тарифы  </a> </Li>
+              <Li><a href="about.html" style="color:black;padding-right: 1rem;"> Контакты   </a> </Li>
+            </ul>
+          </div>
+          <div class="col-sm center">   
+            <p style="font-size:12px;" >Халеминец Дмитрий Сергеевич 
+              ИНН: 742406352530
+              Адрес: 620025, Россия, Свердловская обл, г. Екатеринбург, ул. Бахчиванджи, д. в/ч б-25
+              р/с 40802810500001385230
+              АО "ТИНЬКОФФ БАНК" г. Москва
+              БИК 044525974
+              к/с 30101810145250000974</p>
+            
+          </div>
+          <div class="col-sm" style="margin-left: 3rem; font-size:12px;">
+            <div class="button btn btn-danger">Обратный звонок</div>  <br/> 
+            <p style="margin-top: 0.4rem;">
+            Горячая линия: ********<br/>   
+             Email: expertpravo24@yandex.ru<br/>   
+             Режим работы 24/7<br/>   
+            </p>   
+          </div>
+        </div>
+      </div>
+  
+  
+      <div class="container-fluid text-center" style="margin-bottom:-35px;">
+        <hr>
+        <small>Copyright © 2020⠀⠀⠀ ExpertPravo24.ru   ⠀⠀⠀ Все права защищены.<br/></small>
+        <br/>
+      </div>
+    </div>
+    </footer>
+  <!--FOOTER-->
+
+    <!-- jQuery CDN - Slim version (=without AJAX) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
+    <script src="app.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/typeit@7.0.4/dist/typeit.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+            });
+        });
+    </script>
+</body>
+
+</html>
---
+++ header2.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ExpertPravo</title>  
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"> 
+   <link href='https://fonts.googleapis.com/css?family=Raleway:400,700,500' rel='stylesheet' type='text/css'>  
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+    integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:100,300,400,700" rel="stylesheet">
+    <link href = "https://fonts.googleapis.com/css2? family = Raleway: wght @ 200 & family = Roboto + Mono: wght @ 600 & display = swap" rel = "stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="baza.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+</head>
+
+<style>
+    h3{
+        color:black!important;
+    }
+    .center{
+        text-align:center;
+        margin-left: auto;
+        margin-right:auto;
+    }
+    nav a{
+        font-size: 18px!important;
+        margin-left: 12px;
+    }
+</style>
+
+<body>
+    <div class="wrapper">
+        <!-- Sidebar  -->
+        <nav id="sidebar">
+            <div class="sidebar-header">
+                <h3 class="center">Полная </br>база знаний</h3>
+            </div>
+
+            <ul class="list-unstyled components">
+                <p class="center">Выберите подпункт</p>
+
+                <li class="active">
+                    <a href="#1" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">1. Чарджбэк</a>
+                    <ul class="collapse list-unstyled" id="1">
+                        <li><a href="http://expertpravo.ru/baza_znaniy.php">1.1 ЧТО ТАКОЕ ЧАРДЖБЭК?</a></li>
+                        <li><a href="1.2.php">1.2 Коды чарджбэк</a></li>
+                        <li><a href="1.3.php">1.3 Как сделать чарджбэк?</a></li>
+                        <li><a href="1.4.php">1.4 Заявление на чарджбэк</a></li>
+                        <li><a href="1.5.php">1.5 Вероятность успеха.</a></li>
+                        <li><a href="1.6.php">1.6 Чарджбэк по мошенничеству</a></li>
+                        <li><a href="1.7.php">1.7 Стоимость чарджбэк</a></li>
+                        <li><a href="1.8.php">1.8 Как выбрать Компанию на чарджбэк?</a></li>
+                        <li><a href="1.9.php">1.9 Чарджбэк отзывы</a></li>
+                        <li><a href="1.10.php">1.10 Чарджбэк без предоплаты</a></li>
+                        <li><a href="1.11.php">1.11 Чарджбэк с Алиэкспресс</a></li>
+                        <li><a href="1.12.php">1.12 Как оформить чарджбэк?</a></li>
+                    </ul>
+                </li>
+                
+
+                <li>
+                    <a href="#2" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">2. Платежные системы</a>
+                    <ul class="collapse list-unstyled" id="2">
+                        <li><a href="2.1.php">2.1 Диспут МИР</a></li>
+                        <li><a href="2.2.php">2.2 Чарджбэк VISA</a></li>
+                        <li><a href="2.3.php">2.3 Чарджбэк MasterCard</a></li>
+                        <li><a href="2.4.php">2.4 PayPal</a></li>
+                        <li><a href="2.5.php">2.5 Яндекс Деньги</a></li>
+                        <li><a href="2.6.php">2.6 Чарджбэк и QIWI</a></li>
+                        <li><a href="2.7.php">2.7 Электр. кошельки и др.</a></li>
+                    </ul>
+                </li>
+
+
+                <li>
+                    <a href="#3" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">3. Банки</a>
+                    <ul class="collapse list-unstyled" id="3">
+                        <li><a href="#">3.1 Чарджбэк и Банк Онлайн</a></li>
+                        <li><a href="#">3.2 Чарджбэк Сбербанк</a></li>
+                        <li><a href="#">3.3 Чарджбэк ВТБ</a></li>
+                        <li><a href="#">3.4 Чарджбэк Альфа-банк</a></li>
+                        <li><a href="#">3.5 Чарджбэк Кукуруза</a></li>
+                        <li><a href="#">3.6 Чарджбэк Тинькофф</a></li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="#4" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">4. Брокеры</a>
+                    <ul class="collapse list-unstyled" id="4">
+                        <li><a href="4.1.php">4.1 Кто такие брокеры мошенники</a></li>
+                        <li><a href="4.2.php">4.2 Вернуть деньги из бинарных опционов</a></li>
+                        <li><a href="4.3.php">4.3 Обман брокеров</a></li>
+                        <li><a href="4.4.php">4.4 Сайты близнецы</a></li>
+                        <li><a href="4.5.php">4.5 Брокеры</a></li>
+                        <li><a href="4.6.php">4.6 10 уловок Форекс Брокеров</a></li>
+                        <li><a href="4.7.php">4.7 Зеркальные транзакции</a></li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="#5" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">5. Прочее</a>
+                    <ul class="collapse list-unstyled" id="5">
+                        <li><a href="#">5.1 Mytriumf – ########</a></li>
+                        <li><a href="#">5.2 Частые вопросы</a></li>
+                        <li><a href="#">5.3 Кто такая ###########</a></li>
+                        <li><a href="#">5.4 Как не попасть дважды</a></li>
+                        <li><a href="5.5.php">5.5 AnyDesk(АниДеск) – что это такое?</a></li>
+                        <li><a href="#">5.6 Мошенничество в интернете</a></li>
+                        <li><a href="#">5.7 ##### отзывы</a></li>
+                        <li><a href="#">5.8 Обман пенсионеров</a></li>
+                        <li><a href="#">5.9 Заработок в интернете</a></li>
+                        <li><a href="#">5.10 Банкротство физ. лиц</a></li>
+                        <li><a href="#">5.11 Возврат денег за авиабилет</a></li>
+                        <li><a href="5.12.php">5.12 Что такое МСС код? Всё о МСС!</a></li>
+                        <li><a href="#">5.13 Как вернуть деньги за путевку</a></li>
+                        <li><a href="#">5.14 Потерял инвестиции</a></li>
+                        <li><a href="#">5.15 Словарь терминов</a></li>
+                    </ul>
+                </li>       
+        </nav>
+
+        <!-- Page Content  -->
+        <div id="content">
+            <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
+
+        <div class="container-fluid">
+
+        <button type="button" id="sidebarCollapse" class="btn btn-info">
+            <i class="fas fa-align-left"></i>
+            <span>Меню</span>
+        </button>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+                       
+                <div class="collapse navbar-collapse text-nav" id="navbarTogglerDemo02">
+                <ul class="navbar-nav ml-auto mt-2 mt-lg-0" style="margin-right:3%;  font-family: 'Roboto Slab', serif;">
+                    <li class="nav-item ">
+                    <a class="nav-link" href="index.html">Главная<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="tartifs.html">Тарифы</a>
+                    </li>
+                    <li class="nav-item active" >
+                        <a class="nav-link" href="baza.php">База знаний</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.html">Контакты</a>
+                    </li>
+                </ul>
+                </div>
+            </nav>

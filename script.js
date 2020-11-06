e27b80f033755d1acaef58976eeee57e4771cf50
MaxLestor <MaxLestor@github.com>
2020-11-07 00:28:29 +0600

    выа
    ываываы

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
-$(document).ready(function () {
-    var scroll_start = 0;
-    var startchange = $(".baton");
-    var offset = startchange.offset();
 
-    if (startchange.length) {
-        $(document).scroll(function () {
-            scroll_start = $(this).scrollTop();
-            if (scroll_start > offset.top) {
-                $("nav").css('background-color', 'rgba(0,0,0,.75)');
-            } else {
-                $('nav').css('background-color', 'transparent');
-            }
-        });
+$('.accordion-item .heading').on('click', function(e) {
+    e.preventDefault();
+  
+    // Add the correct active class
+    if($(this).closest('.accordion-item').hasClass('active')) {
+        // Remove active classes
+        $('.accordion-item').removeClass('active');
+    } else {
+        // Remove active classes
+        $('.accordion-item').removeClass('active');
+  
+        // Add the active class
+        $(this).closest('.accordion-item').addClass('active');
     }
-});
-/*$('.type-it').typeIt({
-    strings: ['Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad aspernatur voluptates delectus aliquid facilis! Unde repudiandae ducimus est voluptatem, mollitia necessitatibus vel iusto nostrum voluptate molestias quod illum ipsa distinctio culpa, praesentium aut nobis nulla possimus ea. Quaerat adipisci similique temporibus consequuntur! Sint, error quo. Esse, enim rerum commodi laboriosam soluta a eveniet at, provident impedit corrupti, autem sit.']
-});
-new TypeIt("#simpleUsage", {
-    strings: "This is a simple string.",
-    speed: 50,
-    waitUntilVisible: true
-  }).go();*/
-  // object-fit polyfill run
+  
+    // Show the content
+    var $content = $(this).next();
+    $content.slideToggle(100);
+    $('.accordion-item .content').not($content).slideUp('fast');
+  });

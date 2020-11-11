ce0c2c531ac8fd21cd3e6215aafddf0ea561d01a
MaxLestor <MaxLestor@github.com>
2020-11-12 01:28:16 +0600

    form
    from porn

---
+++ form.php
@@ -0,0 +1,20 @@
+
+        <form>
+          <div class="form-group">
+            <label for="formGroupExampleInput">Ваше имя</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Иван Иванов" required>
+          </div>
+
+          <div class="form-group">
+            <label for="formGroupExampleInput">Номер телефона</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="+7 (495) 755 69 83" required>
+          </div>
+          
+          <div style="color:rgb(128, 119, 119);font-size: 13px;" >
+           Нажимая на кнопку, я согласен(а) с условиями</br><a href="#" style="text-decoration: none; color:rgb(61, 59, 59); font-size: 10px;">
+            Пользовательского соглашения, Клиентского соглашения и Terms and Conditions</a> 
+        </br></br>
+      </div>
+          <button type="submit" class="btn btn-primary btn-lg btn-block">Обратиться</button>
+        </form>
+
